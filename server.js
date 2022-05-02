3db1a6a8a0734edd230c802ea2f0396f15f2bf11
Andeus <andorekun26@gmail.com>
2022-05-02 11:41:25 +0800

    Added error handling, parsing of string to date, parsed utc string to int, improved software design WIP, added empty req.params.id route

--- server.js
+++ server.js
@@ -8,17 +8,33 @@ app.get("/", (req, res) => {
     res.send("Working!")
 })
 
+app.get("/api/", (req, res) => {
+    const dateToUnix = Date.now()
+    const formattedUnix = dateToUnix / 1
+    const date = new Date(formattedUnix)
+      return res.json({"unix": dateToUnix, "utc": date.toUTCString()})
+  })
+
 app.get("/api/:id", (req, res) => {
 
-    if(req.params.id.includes("-")){
-        const dateToUnix = Date.parse(req.params.id)
-        const date = new Date(dateToUnix / 1)
-        res.json({"unix": dateToUnix, "utc": date.toUTCString()})
-    }
-    else{
+   if(/^\d*$/.test(req.params.id)){
     const formattedUnix = req.params.id / 1
     const date = new Date(formattedUnix)
-    res.json({"unix" : req.params.id, "utc": date.toUTCString()})
+      
+      // Error handler
+      if(date.toUTCString() === "Invalid Date") return res.json({"error": "Invalid Date"})
+      
+    res.json({"unix" : parseInt(req.params.id), "utc": date.toUTCString()})
+    }
+
+  else{
+        const dateToUnix = Date.parse(req.params.id)
+        const date = new Date(dateToUnix / 1)
+
+        // Error handler
+        if(date.toUTCString() === "Invalid Date") return res.json({"error": "Invalid Date"})
+
+        res.json({"unix": dateToUnix, "utc": date.toUTCString()})
     }
     
 })
